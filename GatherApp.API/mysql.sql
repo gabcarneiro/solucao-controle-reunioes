d9a43953b4aa59af451a705c433aaf2f8f94abb9
Gabriel Carneiro <gabcarneiro@outlook.com>
2018-06-15 19:34:41 -0300

    Updated sql script

--- GatherApp.API/mysql.sql
+++ GatherApp.API/mysql.sql
@@ -1,4 +1,12 @@
-﻿CREATE TABLE `__EFMigrationsHistory` (
+﻿CREATE DATABASE gatherApp;
+
+USE gatherApp;
+
+CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON * . * TO 'appuser'@'localhost';
+
+CREATE TABLE `__EFMigrationsHistory` (
     `MigrationId` varchar(95) NOT NULL,
     `ProductVersion` varchar(32) NOT NULL,
     CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
